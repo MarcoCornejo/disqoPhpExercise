f2d3b730e5ebbf16892bde08eb3596fb047f9d81
Marco Cornejo <alejandroc_89@hotmail.com>
2019-06-11 15:32:40 -0700

    Note seeder
    Table seeder for dummy data as Notes

---
+++ PhpExercise/database/seeds/NotesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Note;
+
+class NotesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Note::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        for($i=0; $i<50; $i++){
+        	Note::create([
+        		'title' => $faker->word,
+        		'body' => $faker->paragraph,
+        	]);
+        }
+    }
+}
