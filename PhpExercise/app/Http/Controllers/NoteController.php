21545aae8bbfad48e27bc518126f0bf60e19e7fe
Marco Cornejo <alejandroc_89@hotmail.com>
2019-06-11 15:44:30 -0700

    Created NoteController
    Note controller creation, prepared for implicit route model binding

---
+++ PhpExercise/app/Http/Controllers/NoteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    public function index(){
+    	return Note::all();
+    }
+
+    public function show(Note $note){
+    	return $note;
+    }
+
+    public function store(Request $request){
+    	$note = Note::create($request->all());
+
+    	return response()->json($note, 201);
+    }
+
+    public function update(Request $request, Note $note){
+    	$note->update($request->all());
+
+    	return response()->json($note, 200);
+    }
+
+    public function delete(Note $note){
+    	$note->delete();
+
+    	return response()->json(null, 204);
+    }
+}
