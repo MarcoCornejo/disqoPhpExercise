debac02650dfbe0708611e5a9a4dcae9bcf83d65
Marco Cornejo <alejandroc_89@hotmail.com>
2019-06-11 18:19:01 -0700

    Get active user

--- PhpExercise/app/Http/Controllers/NoteController.php
+++ PhpExercise/app/Http/Controllers/NoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Note;
+use Auth;
 
 class NoteController extends Controller
 {
@@ -16,9 +17,11 @@ class NoteController extends Controller
     }
 
     public function store(Request $request){
+        $user = Auth::guard('api')->user();
         $validatedData = $request->validate([
             'title' => 'required|max:50',
             'body' => 'max:1000',
+            'user_email' => $user->email,
         ]);
 
     	$note = Note::create($request->all());
@@ -27,9 +30,11 @@ class NoteController extends Controller
     }
 
     public function update(Request $request, Note $note){
+        $user = Auth::guard('api')->user();
         $validatedData = $request->validate([
             'title' => 'required|max:50',
             'body' => 'max:1000',
+            'user_email' => $user->email,
         ]);
 
     	$note->update($request->all());
